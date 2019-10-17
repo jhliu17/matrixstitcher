92699100bd5a1a8481e5316be2e304f78c446b17
JunHao Liu <junhaoliu17@gmail.com>
2019-10-17 15:43:40 +0800

    Clean duplicated example

--- LUFactorization.py
+++ LUFactorization.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matrixstitcher as mats 
-from matrixstitcher.transform import *
+from matrixstitcher.method import LUFactorization
 
 
 # Edit your matrix to be factorized, here are two examples
@@ -9,57 +9,21 @@ A = [[1, 2, -3, 4], [4, 8, 12, -8], [2, 3, 2, 1], [-3, -1, 1, -4]]
 display_execution = True
 
 
-def lu_factorization(input_matrix):
-    if not input_matrix.square:
-        raise Exception('Plz input a square matrix')
-
-    row_num = input_matrix.rows
-    L = mats.Matrix(np.zeros(input_matrix.shape))
-    P = mats.Matrix(np.eye(row_num))
-
-    for i in range(1, row_num):
-        
-        # check and change the pivot
-        non_zero = i
-        while input_matrix[non_zero, i].to_scalar() == 0.0:
-            non_zero += 1
-            if non_zero > row_num:
-                raise Exception('cannot be fatorized')
-
-        if non_zero != i:
-            row_swap = RowSwap(i, non_zero)
-            input_matrix = row_swap(input_matrix)
-            P = row_swap(P)
-            L = row_swap(L)
-        
-        # reduce row echelon form
-        for j in range(i+1, row_num+1):
-            k = input_matrix[j, i] / input_matrix[i, i]
-            k = k.to_scalar()
-            if k != 0.0:
-                input_matrix = input_matrix.apply(RowTransform(i, -k, j))
-                L[j, i] = k # generate L's element
-    
-    L = L + np.eye(L.rows)
-    U = input_matrix
-    return P, L, U
-
-
 if __name__ == '__main__':
-    A = mats.Matrix(A, dtype=np.float)
-    P, L, U = lu_factorization(A)
+    A = mats.Matrix(A)
+    P, L, U = LUFactorization()(A)
 
     print('\n', '*'*80, sep='')
-    print('Execution Path:\n')
-    reduced_row_echelon_form = A.forward(display=display_execution)
+    print('Execution Path:')
+    result = A.forward(display=display_execution)
     
     print('\n', '*'*80, sep='')
-    print('The LU Factorization result:\n')
-    print('P:\n', P, sep='')
-    print('L:\n', L, sep='')
-    print('U:\n', U, sep='')
+    print('The LU Factorization result:')
+    print('P:\n', P)
+    print('L:\n', L)
+    print('U:\n', U)
 
     print('\n', '*'*80, sep='')
-    print('Proof P * A = L * U:\n')
-    print('P * A:\n', P * A, sep='')
-    print('L * U:\n', L * U, sep='')
+    print('Proof P * A = L * U:')
+    print('P * A:\n', P * A)
+    print('L * U:\n', L * U)
--- LeastSquare.py
+++ LeastSquare.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matrixstitcher as mats
-from matrixstitcher.transform import Inverse, LeastSquareTech
+from matrixstitcher.transform import Inverse
+from matrixstitcher.method import LeastSquareTech
 import matplotlib.pyplot as plt
 
 
@@ -15,19 +16,17 @@ X2 = np.stack([np.ones(x.shape), x, x*x], axis=1)
 X2 = mats.Matrix(X2)
 y = mats.Matrix(y)
 
-def least_square(X, y):
-    res = Inverse()(X.T*X)*(X.T)*y
-    predict = X * res 
-    error = (predict - y).T * (predict - y)
-    return res, predict, error
 
 tech1 = LeastSquareTech()
 alpha1 = tech1(X1, y)
 print(alpha1[0], alpha1[1])
 plt.plot(x, tech1.predict(X1).numpy(), c='r')
+
+
 tech2 = LeastSquareTech()
 alpha2 = tech2(X2, y)
 print(alpha2[0], alpha2[1])
 plt.plot(x, tech2.predict(X2).numpy(), c='b')
+
 plt.grid(True)
 plt.show()
