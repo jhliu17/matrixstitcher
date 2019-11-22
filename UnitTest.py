f54f5f33e4e9404fbbae2fbe154488aab48f1f80
JunHao Liu <junhaoliu17@gmail.com>
2019-11-22 20:36:03 +0800

    QR Factorization

--- UnitTest.py
+++ UnitTest.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matrixstitcher as mats
-from matrixstitcher.method import Rotator, Reflector, HouseHold, Givens, GramSchmidt
+from matrixstitcher.method import Rotator, Reflector, HouseHolder, Givens, GramSchmidt
 
 
 v = mats.Matrix([[0, -20, -14], [3, 27, -4], [4, 11, -2]], dtype=np.float64)
 Q1, R1 = GramSchmidt()(v)
-Q2, R2 = HouseHold()(v)
+Q2, R2 = HouseHolder()(v)
 Q3, R3 = Givens()(v)
 print(Q1 * R1)
 print(Q2 * R2)
